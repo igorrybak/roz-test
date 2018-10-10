e297ab4db40ffc179bcdd27dc04c324d850e781a
Igor <hostel@ukr.net>
2018-10-10 23:22:06 +0300

    Path to driver

--- src/test/java/CheckRozetkaFindFieldTest.java
+++ src/test/java/CheckRozetkaFindFieldTest.java
@@ -12,8 +12,8 @@ public class CheckRozetkaFindFieldTest {
     public void search_field_is_work() {
         WebDriver browser;
         browser = new ChromeDriver();
-        browser.get("https://rozetka.com.ua/");
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Igor\\Documents\\Java\\chromedriver\\chromedriver.exe");
+        browser.get("https://rozetka.com.ua");
+        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver\\chromedriver.exe");
         WebElement searchField = browser.findElement(By.xpath("/html[1]/body[1]/header[1]/div[1]/div[1]/div[2]/div[3]/form[1]/div[1]/div[2]/input[1]"));
         WebElement searchButton = browser.findElement(By.xpath("/html[1]/body[1]/header[1]/div[1]/div[1]/div[2]/div[3]/form[1]/span[1]/span[1]/button[1]"));
         searchField.sendKeys("компьютер");
