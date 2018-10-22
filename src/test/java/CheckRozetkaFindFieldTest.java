2c09845854412d320e8b74762a9ab4f8974ab938
Igor <hostel@ukr.net>
2018-10-22 22:13:46 +0300

    Cucumber with Gherkin added Begin writing feature and scenario

--- src/test/java/CheckRozetkaFindFieldTest.java
+++ src/test/java/CheckRozetkaFindFieldTest.java
@@ -1,5 +1,3 @@
-//import static org.junit.jupiter.api.Assertions.*;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
---
+++ src/test/java/RunCucumberTest.java
@@ -0,0 +1,11 @@
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+
+public class RunCucumberTest {
+    @RunWith(Cucumber.class)
+    @CucumberOptions(plugin = {"pretty"})
+    public class RunCukesTest {
+    }
+}
