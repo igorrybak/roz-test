d029e35bf62b89b0742e6180a247b8bffcf99e04
Igor <hostel@ukr.net>
2018-10-22 15:57:54 +0300

    OS check added

--- src/test/java/CheckRozetkaFindFieldTest.java
+++ src/test/java/CheckRozetkaFindFieldTest.java
@@ -1,4 +1,5 @@
 //import static org.junit.jupiter.api.Assertions.*;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -6,15 +7,19 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.jupiter.api.Test;
 
-
 public class CheckRozetkaFindFieldTest {
     @Test
     public void search_field_is_work() {
-        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver\\chromedriver.exe");
+        if (OSValidator.isUnix()) {
+            System.setProperty("webdriver.chrome.driver", "/home/igor/soft/chromedriver/chromedriver");
+        }
+        if (OSValidator.isWindows()) {
+            System.setProperty("webdriver.chrome.driver", "D:\\chromedriver\\chromedriver.exe");
+        }
         WebDriver browser;
         browser = new ChromeDriver();
         browser.get("https://rozetka.com.ua");
-        WebElement searchField = browser.findElement(By.xpath("//input[@placeholder='Поиск']"));
+        WebElement searchField = browser.findElement(By.xpath("//input[@class='rz-header-search-input-text passive']"));
         WebElement searchButton = browser.findElement(By.xpath("//button[@class='btn-link-i js-rz-search-button']"));
         searchField.sendKeys("компьютер");
         searchButton.sendKeys(Keys.RETURN);
@@ -22,3 +27,4 @@ public class CheckRozetkaFindFieldTest {
         browser.close();
     }
 }
+
